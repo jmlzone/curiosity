0b7e3d162b54a4d324b3d222c7837b2d8a0afa9f
James Lee <jml@jmlzone.com>
2017-06-04 13:31:50 -0400

    camera support

--- rover/python/taskRun.py
+++ rover/python/taskRun.py
@@ -7,13 +7,43 @@ import sys
 import time
 import RPi.GPIO as GPIO
 import sensors
+import camera
+import socket
+
+def sequenceNumber():
+    sequencePath = configRoot + "/sequence.txt"
+    if os.path.isfile(sequencePath):
+        file = open(sequencePath)
+        sequence = file.read()
+        file.close()
+        sequence = str(int(sequence) + 1)
+    else:
+        sequence = "1"
+    file = open(sequencePath,"w")
+    file.write(sequence)
+    file.close()
+    return sequence
+
+
+hostname = socket.gethostname()
+htmlRoot = "/var/www/html/curiosity"
+configRoot = htmlRoot + "/missions"
+htmlBaseName = htmlRoot + "/" + hostname + "_"
+
+seq = sequenceNumber()
 
 c = chassis.chassis()
 s = sensors.sensors()
+cam = camera(hostname,seq,htmlBaseName,40)
+
 
 argNum = 1;
 while (argNum < len(sys.argv)) :
-    c.run(sys.argv[argNum], sys.argv[argNum+1])
+    task = sys.argv[argNum]
+    if(task.find("cam") != -1):
+        cam.taskCapture(task, sys.argv[argNum+1])
+    else:
+        c.run(task, sys.argv[argNum+1])
     s.readAll()
     argNum = argNum + 2
 
